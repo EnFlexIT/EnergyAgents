982f7b2f5d39c8011e664aaa7506d6132f55774f
CDerksen <christian.derksen@uni-due.de>
2023-10-18 19:05:43 +0200

    Added base of bundle de.enflexit.ea.core.configuration to feature

---
+++ eclipseProjects/de.enflexit.ea/bundles/de.enflexit.ea.core.configuration/src/de/enflexit/ea/core/configuration/SetupConfigurationService.java
@@ -0,0 +1,11 @@
+package de.enflexit.ea.core.configuration;
+
+/**
+ * The Interface SetupConfigurationService.
+ *
+ * @author Christian Derksen - SOFTEC - ICB - University of Duisburg-Essen
+ */
+public interface SetupConfigurationService {
+
+	
+}
